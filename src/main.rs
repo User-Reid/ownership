29ff909efc1993ebb391e6511a4467e672e28d9c
Reid Herrera <reidherrerabusiness@gmail.com>
2025-01-25 11:36:58 -0600

    working with String type

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 fn main() {
-    let time: i32 = 2025;
-    let year: i32 = time;
-
-    println!("The time is {time}. It is the year {year}")
+    let food: &str = "pasta";
+    let text: String = String::new();
+    let candy: String = String::from("KitKat")
 }
