8e25046248a99ffc863daecdb0128775a43be3ac
Reid Herrera <reidherrerabusiness@gmail.com>
2025-01-26 15:15:55 -0600

    understanding mutable paramaters

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,9 @@
-fn print_my_value(value: String) {
-    println!("You value is {value}")
-}
-
-fn example(x: i32) {
-    println!("{x}")
+fn add_fries(mut meal: String) {
+    meal.push_str(" and Fries");
+    println!("{meal}")
 }
 
 fn main() {
-    let apples: String = String::from("Oranges");
-    let x: i32 = 32;
-    println!("{apples}");
-    print_my_value(apples);
-    example(x);
+    let burger: String = String::from("Burger");
+    add_fries(burger);
 }
