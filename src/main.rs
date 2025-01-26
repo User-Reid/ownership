ae7c2befee5bcd23543ced476157d8024d3db3f8
Reid Herrera <reidherrerabusiness@gmail.com>
2025-01-26 11:55:11 -0600

    understanding dereference operators and its use case.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
     let my_stack_value: i32 = 2;
     let my_intiger_reference: &i32 = &my_stack_value;
+    println!("{}", *my_intiger_reference);
+    println!("{my_intiger_reference}");
 
     let my_heap_value: String = String::from("Toyota");
     let my_heap_reference: &String = &my_heap_value;
