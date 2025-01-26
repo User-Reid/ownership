2f9205ae58fccc5a8517fd2418d671f7e36696e8
Reid Herrera <reidherrerabusiness@gmail.com>
2025-01-26 14:53:09 -0600

    went over the copy trait and ownership

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
-fn main() {
-    let x: &str = "tacos";
+fn print_my_value(value: String) {
+    println!("You value is {value}")
+}
 
+fn example(x: i32) {
     println!("{x}")
+}
+
+fn main() {
+    let apples: String = String::from("Oranges");
+    let x: i32 = 32;
+    println!("{apples}");
+    print_my_value(apples);
+    example(x);
 }
