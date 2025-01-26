d511285abe8ad3d6767fbac59a88410a7e6a0a0c
Reid Herrera <reidherrerabusiness@gmail.com>
2025-01-26 13:03:44 -0600

    understanding difference between string types

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,5 @@
 fn main() {
-    let my_stack_value: i32 = 2;
-    let my_intiger_reference: &i32 = &my_stack_value;
-    println!("{}", *my_intiger_reference);
-    println!("{my_intiger_reference}");
+    let x: &str = "tacos";
 
-    let my_heap_value: String = String::from("Toyota");
-    let my_heap_reference: &String = &my_heap_value;
-    println!("{my_heap_reference}");
-
-    let potato: i32 = 2;
-    println!("{potato}");
+    println!("{x}")
 }
