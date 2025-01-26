77c950bc966215d28094e45e4331f33be26917df
Reid Herrera <reidherrerabusiness@gmail.com>
2025-01-25 21:42:32 -0600

    went over refrences and borrowing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 fn main() {
-    let person: String = String::from("Reid");
-    let genius: String = person.clone();
+    let my_stack_value: i32 = 2;
+    let my_intiger_reference: &i32 = &my_stack_value;
 
-    println!("This is {person}");
+    let my_heap_value: String = String::from("Toyota");
+    let my_heap_reference: &String = &my_heap_value;
+    println!("{my_heap_reference}");
+
+    let potato: i32 = 2;
+    println!("{potato}");
 }
